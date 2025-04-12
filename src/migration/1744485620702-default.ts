c0fa6f0ffa2919ca4671b3c3f87fbbf5ac39c659
Marcos Prado <marcospradofelix@gmail.com>
2025-04-12 16:33:01 -0300

    feat: add migrations to create USERS table

---
+++ src/migration/1744485620702-default.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Default1744485620702 implements MigrationInterface {
+    name = 'Default1744485620702'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "USERS" ("id" character varying NOT NULL, "name" character varying(50) NOT NULL, "email" character varying(50) NOT NULL, "password" character varying NOT NULL, "birth_date" TIMESTAMP NOT NULL, "created_at" TIMESTAMP NOT NULL, CONSTRAINT "PK_b16c39a00c89083529c6166fa5b" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "USERS"`);
+    }
+
+}
