4e21ad3c9ba98917e571aca8db35e246c5facfdc
Marcos Prado <marcospradofelix@gmail.com>
2025-04-12 16:29:39 -0300

    feat: implement user registration in UserController with validation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,35 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
+import {UserService} from "../services/user/user.service";
+import {plainToInstance} from "class-transformer";
+import {UserRequestDto} from "../dto/user-request.dto";
+import {validate} from "class-validator";
 
 export class UserController {
+    constructor(
+        private readonly userService: UserService,
+    ) {
+    }
+
+    async registerUser(req: Request, res: Response) {
+        const dto = plainToInstance(UserRequestDto, req.body) as unknown as UserRequestDto;
+
+        const errors = await validate(dto);
+
+        if (errors.length > 0) {
+            return res.status(400).json({errors});
+        }
+
+        try {
+            res.status(201).json(await this.userService.registerUser(dto));
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(500).json({ message: 'Unexpected error' });
+            }
+        }
+    }
+
     public getUsers(_: Request, res: Response): Response {
         return res.status(200).json({
             status: 'success',
