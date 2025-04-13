aa3458570a1ee1f3425bfded63a5911eb64eab09
Marcos Prado <marcospradofelix@gmail.com>
2025-04-13 19:00:39 -0300

    feat: add vehicle routes to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import usersRoutes from "./users.routes";
+import vehicleRoutes from "./vehicles.routes";
 
 export const router: Router = Router()
-    .use("/users", usersRoutes);
+    .use("/users", usersRoutes)
+    .use("/vehicles", vehicleRoutes);
