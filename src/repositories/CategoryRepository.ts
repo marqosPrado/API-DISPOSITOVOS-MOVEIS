23ee94551ee3142fccdc320cc70e71426bc60fdb
Marcos Prado <marcospradofelix@gmail.com>
2025-04-21 19:20:18 -0300

    feat: implement Category module with routes, controller, service, and repository

---
+++ src/repositories/CategoryRepository.ts
@@ -0,0 +1,21 @@
+import {Repository} from "typeorm";
+import {CategoryAggregate} from "../aggregates/CategoryAggregate";
+import {AppDataSource} from "../config/database/data-source";
+import {Category} from "../domain/Category/Category";
+
+export class CategoryRepository {
+    private repository: Repository<CategoryAggregate> = AppDataSource.getRepository(CategoryAggregate);
+
+    async save(category: CategoryAggregate): Promise<Category> {
+        const savedAggregate: CategoryAggregate = await this.repository.save(category);
+        return savedAggregate.toDomain();
+    }
+
+    async findOneByName(name: string): Promise<Category | null> {
+        const categoryAggregate: CategoryAggregate | null = await this.repository.findOneBy({ name: name })
+        if (!categoryAggregate) {
+            return null;
+        }
+        return categoryAggregate.toDomain();
+    }
+}
